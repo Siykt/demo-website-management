49e1adda25911f5201dfedfd80bdff97d8617efa
siykt <cnsiykt@163.com>
2022-05-13 14:51:20 +0800

    feat: 添加layout

---
+++ layout/default.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import type { MenuProps } from 'antd'
+import { Breadcrumb, Layout, Menu } from 'antd'
+import { DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons'
+import { ConfigResponseData } from '../pages/api/config'
+
+const { Header, Content, Footer, Sider } = Layout
+
+type MenuItem = Required<MenuProps>['items'][number]
+
+function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode, children?: MenuItem[]): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label
+  } as MenuItem
+}
+
+const items: MenuItem[] = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [getItem('Tom', '3'), getItem('Bill', '4'), getItem('Alex', '5')]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileOutlined />)
+]
+
+interface Props {
+  children?: React.ReactNode
+  config?: ConfigResponseData
+}
+
+export default function DefaultLayout({ children, config }: Props) {
+  const [collapsed, setCollapsed] = useState(false)
+  const onCollapse = (collapsed: boolean) => {
+    console.log(collapsed)
+    setCollapsed(collapsed)
+  }
+
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            {children}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          {config?.icp && <span>{config.icp}</span>}
+          <span>{config?.copyright || 'Siykt ©2022'}</span>
+        </Footer>
+      </Layout>
+    </Layout>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
-import '../styles/bootstrap.min.css'
-import '../styles/globals.scss'
+// import '../styles/bootstrap.min.css'
+// import '../styles/globals.scss'
+import 'antd/dist/antd.css'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import Layout from '../layout/default'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type NextPageWithLayout = NextPage & {
+  hideLayout?: boolean
 }
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  return (
+    <>
+      {Component.hideLayout ? (
+        <Component {...pageProps} />
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </>
+  )
+}
